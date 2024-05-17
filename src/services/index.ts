02c44c969b98c18db9476ddc15d439ac344ac978
joao-trajano <joao.trajanosouza@gmail.com>
2024-05-17 13:03:22 -0300

    refector: axios changed to fetchData and validating return type for model

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,20 @@
-import axios from 'axios'
+enum API {
+  BASE_URL = "https://parallelum.com.br/fipe/api/v1",
+}
 
-const api = axios.create({
-  baseURL: 'https://parallelum.com.br/fipe/api/v1',
-});
+type ApiProps = {
+  path: string;
+  method: "GET" | "POST" | "PUT" | "DELETE";
+};
 
-export default api;
+export const Api = async ({ path, method }: ApiProps): Promise<any> => {
+  try {
+    const res = await fetch(`${API.BASE_URL}/${path}`, {
+      method,
+    });
+
+    return res.json();
+  } catch (error) {
+    throw new Error("Failed to fetch data");
+  }
+};
--- src/services/modelsCar/index.ts
+++ src/services/modelsCar/index.ts
@@ -1,11 +1,28 @@
-import api from '..'
+import { Api } from "..";
 
-export const getAllModelsByCode = async(code: number): Promise<any> => {
+export type Modelo = {
+  codigo: number;
+  nome: string;
+};
+
+export type Ano = {
+  codigo: number;
+  nome: string;
+};
+
+export type Response = {
+  modelos: Modelo[];
+  anos: Ano[];
+};
+
+export const getAllModelsByCode = async (code: number): Promise<Response> => {
   try {
-    const { data } = await api.get(`/carros/marcas/${code}/modelos`)
-    return data;
-
+    const data = await Api({
+      path: `carros/marcas/${code}/modelos`,
+      method: "GET",
+    });
+    return data as Response;
   } catch (error) {
-    console.log(error)
+    throw new Error("Não foi possível listar os modelos!");
   }
-}
+};
